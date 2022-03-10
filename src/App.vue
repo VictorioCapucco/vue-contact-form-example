de1993b3f4de444acb8d66ddea86af34fe459672
Victório Capucço <65973246+VictorioCapucco@users.noreply.github.com>
2022-03-10 10:28:32 -0300

    #2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="contact-form-section">
+      <div class="container">
+        <field width="full-desktop" label="Nome completo">
+          <input />
+        </field>
+        <field width="half-desktop" label="CEP">
+          <input />
+        </field>
+        <field width="half-desktop" label="Endereço">
+          <input disabled class="disabled" />
+        </field>
+        <field width="half-desktop" label="Estado">
+          <input disabled class="disabled" />
+        </field>
+        <field width="half-desktop" label="Cidade">
+          <input disabled class="disabled" />
+        </field>
+        <field width="full-desktop" label="Informe suas dúvidas">
+          <textarea rows="5"></textarea>
+        </field>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import field from "./components/field.vue";
+  export default {
+    components: {
+      field,
+    },
+    name: "App",
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    background-color: #ccc;
+  }
+  input,
+  textarea {
+    width: 100%;
+    margin-top: 10px;
+    border: 1px solid;
+    color: #666;
+    border-radius: 10px;
+    outline: none;
+    padding: 9px 14px;
+    box-sizing: border-box;
+    font-size: 14px;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .contact-form-section {
+    max-width: 750px;
+    margin: auto;
+    padding: 20px;
+  }
+  .disabled {
+    cursor: not-allowed;
+  }
+  .container {
+    display: flex;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 20px;
+    flex-wrap: wrap;
+    background-color: white;
+  }
 </style>
---
+++ src/components/field.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="width" class="column-width">
+    <div class="field">
+      <label>{{ label }}</label>
+      <slot />
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        default: "",
+      },
+      width: {
+        type: String,
+        default: "",
+      },
+    },
+  };
+</script>
+<style>
+  .field {
+    padding: 10px;
+  }
+  .column-width {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 15px;
+  }
+  label {
+    font-size: 18px;
+    font-weight: bold;
+    padding-left: 5px;
+  }
+  @media screen and (min-width: 768px) {
+    .full-desktop {
+      width: 100%;
+    }
+    .half-desktop {
+      width: 50%;
+    }
+  }
+</style>
